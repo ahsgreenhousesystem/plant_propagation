57c532896f9d4a60d41c5a7445dbd63953f8517a
sbajric <sbajric@iastate.edu>
2015-03-03 13:47:48 -0600

    Renamed zones and functions, fixed setup to close zones initially, added logging

--- New Webserver Code/sprinkler/sprinkler.ino
+++ New Webserver Code/sprinkler/sprinkler.ino
@@ -1,41 +1,46 @@
-const int zoneOne   = 23; 
-const int zoneTwo   = 25;
-const int zoneThree = 27;
-const int zoneFour  = 29;
-const int zoneFive  = 31;
-const int zoneSix   = 45;
-const int zoneSeven   = 47;
+const int zone1 = 23; 
+const int zone2 = 25;
+const int zone3 = 27;
+const int zone4 = 29;
+const int zone5 = 31;
+const int zone6 = 45;
+const int zone7 = 47;
 
 const int timeDelay = 10000;     // delay in ms -- important -- relays wear out if driven too fast
 
 void setup()
 {
-  //set zone pins as output 
-//  pinMode(zoneOne,   OUTPUT);  
-  pinMode(zoneTwo,   OUTPUT);
-//  pinMode(zoneThree, OUTPUT);
-//  pinMode(zoneFour,  OUTPUT);
-//  pinMode(zoneFive,  OUTPUT);
-//  pinMode(zoneSix,   OUTPUT);
-//  pinMode(zoneSeven, OUTPUT);
+  //initialize all zone pins
+  pinMode(zone1, OUTPUT);
+  sprinklerOff(zone1);
+  pinMode(zone2, OUTPUT);
+  sprinklerOff(zone2);
+  pinMode(zone3, OUTPUT);
+  sprinklerOff(zone3);
+  pinMode(zone4, OUTPUT);
+  sprinklerOff(zone4);
+  pinMode(zone5, OUTPUT);
+  sprinklerOff(zone5);
+  pinMode(zone6, OUTPUT);
+  sprinklerOff(zone6);
+  pinMode(zone7, OUTPUT);
+  sprinklerOff(zone7);
 }
-
 
 void loop()                    
 {
-  sprinklersOff(zoneTwo);
-  sprinklersOn(zoneTwo);
-//  sprinklersOff(zoneSeven);
-//  sprinklersOn(zoneSix);
+  sprinklerOff(zone2);
+  sprinklerOn(zone2);
 } 
 
-
-void sprinklersOn(int pin) {
+void sprinklerOn(int pin) {
+  Serial.println(pin + " was turned ON."); // will be put in log
   digitalWrite(pin, HIGH);      // turn relay on
   delay(timeDelay);             // wait for one second
 }
 
-void sprinklersOff(int pin) {
+void sprinklerOff(int pin) {
+  Serial.println(pin + " was turned OFF."); // will be put in log
   digitalWrite(pin, LOW);       // turn relay off
   delay(timeDelay);             // wait for one second
 }
