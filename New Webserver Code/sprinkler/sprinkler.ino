4fd3b71e9edaa79026a93d8547b5f88bb18eb28e
jmeis <joemmeis@iastate.edu>
2015-03-04 08:05:16 -0600

    changed some formatting

--- New Webserver Code/sprinkler/sprinkler.ino
+++ New Webserver Code/sprinkler/sprinkler.ino
@@ -6,23 +6,29 @@ const int zone5 = 31;
 const int zone6 = 45;
 const int zone7 = 47;
 
-const int timeDelay = 10000;     // delay in ms -- important -- relays wear out if driven too fast
+const int timeDelay = 5000;                      // delay in ms -- important -- relays wear out if driven too fast
 
 void setup()
 {
   //initialize all zone pins
   pinMode(zone1, OUTPUT);
   sprinklerOff(zone1);
+
   pinMode(zone2, OUTPUT);
   sprinklerOff(zone2);
+
   pinMode(zone3, OUTPUT);
   sprinklerOff(zone3);
+
   pinMode(zone4, OUTPUT);
   sprinklerOff(zone4);
+
   pinMode(zone5, OUTPUT);
   sprinklerOff(zone5);
+
   pinMode(zone6, OUTPUT);
   sprinklerOff(zone6);
+
   pinMode(zone7, OUTPUT);
   sprinklerOff(zone7);
 }
@@ -34,13 +40,13 @@ void loop()
 } 
 
 void sprinklerOn(int pin) {
-  Serial.println(pin + " was turned ON."); // will be put in log
-  digitalWrite(pin, HIGH);      // turn relay on
-  delay(timeDelay);             // wait for one second
+  Serial.println(pin + " was turned ON.");       // will be put in log
+  digitalWrite(pin, HIGH);                       // turn relay on
+  delay(timeDelay);                              // wait for five seconds
 }
 
 void sprinklerOff(int pin) {
-  Serial.println(pin + " was turned OFF."); // will be put in log
-  digitalWrite(pin, LOW);       // turn relay off
-  delay(timeDelay);             // wait for one second
+  Serial.println(pin + " was turned OFF.");      // will be put in log
+  digitalWrite(pin, LOW);                        // turn relay off
+  delay(timeDelay);                              // wait for five seconds
 }
