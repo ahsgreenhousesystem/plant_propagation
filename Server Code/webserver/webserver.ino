3dbf44a04382e22efd574281f66607793b9271a6
jmeis <joemmeis@iastate.edu>
2015-04-15 17:23:00 -0500

    Fixed same problem. Added shorter wait so it doesn't take forever.

--- Server Code/webserver/webserver.ino
+++ Server Code/webserver/webserver.ino
@@ -17,7 +17,7 @@ with an Ethernet shield using the WizNet chipset.
 #define BUFFER_SIZE 64
 #define TABLE_SIZE 16
 
-const int zone1 = 23; 
+const int zone1 = 23;
 const int zone2 = 25;
 const int zone3 = 27;
 const int zone4 = 29;
@@ -25,7 +25,7 @@ const int zone5 = 31;
 const int zone6 = 45;
 const int zone7 = 47;
 
-const int timeDelay = 5000; // delay in ms -- important -- relays wear out if driven too fast
+const int timeDelay = 2000; // delay in ms -- important -- relays wear out if driven too fast
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // MAC address from Ethernet shield sticker under board
 IPAddress ip(10, 222, 1, 250); // 10.222.1.250 is the IP address given by Ames High School Technology Director
@@ -37,7 +37,7 @@ char req_index = 0;              // index into HTTP_req buffer
 unsigned int localPort = 8888;      // local port to listen for UDP packets
 IPAddress timeServer(132, 163, 4, 101); // time-a.timefreq.bldrdoc.gov NTP server
 const int NTP_PACKET_SIZE= 48; // NTP time stamp is in the first 48 bytes of the message
-byte packetBuffer[ NTP_PACKET_SIZE]; // buffer to hold incoming and outgoing packets 
+byte packetBuffer[ NTP_PACKET_SIZE]; // buffer to hold incoming and outgoing packets
 EthernetUDP Udp; // A UDP instance to let us send and receive packets over UDP
 
 //boolean LED_state[4] = {0}; // stores the states of the LEDs
@@ -65,19 +65,32 @@ void setup()
 {
     // Initialize all zone pins
     pinMode(zone1, OUTPUT);
+    sprinklerOff(zone1);
+
     pinMode(zone2, OUTPUT);
+    sprinklerOff(zone2);
+
     pinMode(zone3, OUTPUT);
+    sprinklerOff(zone3);
+
     pinMode(zone4, OUTPUT);
+    sprinklerOff(zone4);
+
     pinMode(zone5, OUTPUT);
+    sprinklerOff(zone5);
+
     pinMode(zone6, OUTPUT);
+    sprinklerOff(zone6);
+
     pinMode(zone7, OUTPUT);
-  
+    sprinklerOff(zone7);
+
     // disable Ethernet chip
     pinMode(10, OUTPUT);
     digitalWrite(10, HIGH);
-    
+
     Serial.begin(19200);       // for debugging
-    
+
     // initialize SD card
     Serial.println("Initializing SD card...");
     if (!SD.begin(4)) {
@@ -91,7 +104,7 @@ void setup()
         return;  // can't find index file
     }
     Serial.println("SUCCESS - Found website/overview.htm file.");
-    
+
     if (!SD.exists("config.db")) {
         Serial.println("ERROR - Can't find config.db file!");
         return;  // can't find index file
@@ -104,9 +117,9 @@ void setup()
 //        Serial.println("SUCCESS - Config database created.");
 //        //might need to initialize all zones here
 //    } else {
-//       Serial.println("Config database already exists."); 
+//       Serial.println("Config database already exists.");
 //    }
-  
+
    /* for(int i=0; i < config_DB.count();i++)
     {
 //        config_DB.readRec(i,EDB_REC zone_config);
@@ -116,9 +129,9 @@ void setup()
     for(int i =0; i< config_DB.count(); i++) {
         Serial.println(zone[i].Name);
     }
-    
+
     */
-    
+
     Ethernet.begin(mac, ip);  // initialize Ethernet device
     server.begin();           // start to listen for clients
     Udp.begin(localPort);
@@ -202,7 +215,7 @@ void loop()
                     // last character on line of received text
                     // starting new line with next character read
                     currentLineIsBlank = true;
-                } 
+                }
                 else if (c != '\r') {
                     // a text character was received from client
                     currentLineIsBlank = false;
@@ -257,7 +270,7 @@ char StrContains(char *str, char *sfind)
 unsigned long sendNTPpacket(IPAddress& address)
 {
   // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE); 
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
   // Initialize values needed to form NTP request
   // (see URL above for details on the packets)
   packetBuffer[0] = 0b11100011;   // LI, Version, Mode
@@ -265,29 +278,29 @@ unsigned long sendNTPpacket(IPAddress& address)
   packetBuffer[2] = 6;     // Polling Interval
   packetBuffer[3] = 0xEC;  // Peer Clock Precision
   // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49; 
+  packetBuffer[12]  = 49;
   packetBuffer[13]  = 0x4E;
   packetBuffer[14]  = 49;
   packetBuffer[15]  = 52;
 
   // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:         
+  // you can send a packet requesting a timestamp:
   Udp.beginPacket(address, 123); //NTP requests are to port 123
   Udp.write(packetBuffer,NTP_PACKET_SIZE);
-  Udp.endPacket(); 
+  Udp.endPacket();
 }
 
 void writer(unsigned long address, byte data)
 {
-  dbFile.seek(address); 
-  dbFile.write(data); 
+  dbFile.seek(address);
+  dbFile.write(data);
   dbFile.flush();
 }
- 
+
 byte reader(unsigned long address)
 {
-  dbFile.seek(address); 
-  return dbFile.read(); 
+  dbFile.seek(address);
+  return dbFile.read();
 }
 
 void sprinklerOff(int pin) {
