6d7cc46c3d0647c02bff1d5fc99034d3563bb65a
kstaines <kstaines@iastate.edu>
2015-04-24 21:36:26 -0500

    started adding .h and .cpp files to create library. At top of webserver.h is link to the example site

---
+++ Server Code/libraries/webserver/webserver.h
@@ -0,0 +1,13 @@
+/*
+	webserver.h - Library for Greenhouse webserver.
+	http://www.arduino.cc/en/Hacking/LibraryTutorial
+*/
+#ifndef webserver_h
+#define webserver_h
+
+#include "Arduino.h"
+
+/* code below */
+
+
+#endif
