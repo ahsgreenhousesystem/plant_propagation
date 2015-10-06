d9377d646b492669d26aa8b0fb5406e5dbe72051
sbajric <sbajric@iastate.edu>
2015-10-06 11:38:05 -0500

    Moved JS code out of html files into their own

---
+++ Server Code/plant_propogation/public/js/config.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+
+    /*
+    Load information into page. 
+         
+    This isn't going to be here.  On load of page, need to supply html pages with data to fill in 
+    all sections of the page. This may have to be done when the page is sent from the server. 
+         
+    You guys have any suggestions on best way to do this?  Probably done in index.js, but not sure how.  
+    */
+
+    $.get("/allZones", function(response) {
+        alert(response);
+
+        var zones = response.body.zones;
+        for (var zone in zones) {
+            var num = zone.zone;
+            var active = document.getElementById('A' + num);
+            active.checked = zone.active;
+            var name = document.getElementById('N' + num);
+            name.text = zone.name;
+
+
+            for (var i = 1; i <= 3; i++) {
+                var begin = document.getElementById('Z' + num + 'T' + i);
+                var end = document.getElementById('Z' + num + 'D' + i);
+                begin.text = zone.times[i - 1].begin;
+                end.text = zone.times[i - 1].end;
+            }
+        }
+
+    });
+
+    $('.input-sm').datetimepicker({
+        format: 'LT'
+    });
+
+    //TODO - change to only buttons for individual zones, add another for 'Update All'
+    $("button").click(function() {
+        var zone = $(this).attr("id");
+        var begin1 = $("#Z" + zone + "T1").val();
+        var end1 = $('#Z' + zone + "D1").val();
+        var begin2 = $('#Z' + zone + "T2").val();
+        var end2 = $('#Z' + zone + "D2").val();
+        var begin3 = $('#Z' + zone + "T3").val();
+        var end3 = $('#Z' + zone + "D3").val();
+
+        $.post("/config", {
+                "zone": zone,
+                "times": [{
+                    "begin": begin1,
+                    "end": end1
+                }, {
+                    "begin": begin2,
+                    "end": end2
+                }, {
+                    "begin": begin3,
+                    "end": end3
+                }]
+            },
+            function(response) {
+                $("#zone" + zone + "response").text(response);
+            });
+    });
+
+    //This is from old zones.html (Setup page)
+    //TODO - have website fill values before load
+    function setupClick(zone) {
+        if (zone) {
+            var name = $("#N" + zone).val();
+            var active = $("#A" + zone).prop("checked");
+
+            $.post("/setup", {
+                "zone": zone,
+                "name": name,
+                "active": active
+            }, function(response) {
+
+            });
+        }
+    };
+});
---
+++ Server Code/plant_propogation/public/js/overview.js
@@ -0,0 +1,21 @@
+$(function() {
+    $("form input[type=submit]").click(function() {
+        $("input[type=submit]", $(this).parents("form")).removeAttr("clicked");
+        $(this).attr("clicked", "true");
+    });
+
+    $("form").submit(function(e) {
+        e.preventDefault();
+        var button = $("input[type=submit][clicked=true]"); // the button that was clicked
+        var zone = button.attr("name"); // button zone name
+        var action = button.val(); // button value (Open/Close/Auto)
+        $.post("/?control" + "&zone=" + zone.substring(4) + "&action=" + action, function(data) { // send AJAX request to the web server
+            $("#" + zone + "log").text(data); // add notification to the log in the zone button panel
+            var zoneButtons = document.getElementsByName(zone); // array of the buttons for that zone
+            for (var i = 0; i < zoneButtons.length; i++) {
+                zoneButtons[i].setAttribute("class", "btn btn-default btn-sm"); // reset all buttons to default
+            }
+            button.addClass("btn-success"); // highlight the button that was clicked
+        });
+    });
+});
