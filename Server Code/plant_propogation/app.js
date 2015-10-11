e673194f1586582bb5678de84d2cad67fe036296
CamJ <camj@iastate.edu>
2015-10-10 20:48:34 -0500

    removed unused imports

--- Server Code/plant_propogation/app.js
+++ Server Code/plant_propogation/app.js
@@ -1,15 +1,10 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var http = require('http').Server(app)
-var path = require('path')
-// udoo = require('udoo')
+// udoo = require('udoo');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+//var users = require('./routes/users');
 
 var app = express();
 
@@ -20,17 +15,6 @@ var db = monk('localhost:27017/plant_propogation');
 
 app.use(express.static(__dirname + '/public'));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our route
 app.use(function (req, res, next) {
@@ -39,7 +23,6 @@ app.use(function (req, res, next) {
 });
 
 app.use('/', routes);
-app.use('/users', users);
 
 /*
 # set pins
