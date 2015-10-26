73d857b624628737724da0547509be9afa0d1c6f
CamJ <camj@iastate.edu>
2015-10-25 22:59:24 -0500

    update time post route and posting times to server

--- Server Code/plant_propogation/routes/index.js
+++ Server Code/plant_propogation/routes/index.js
@@ -5,7 +5,7 @@ var path = require('path');
 /* GET home page. */
 router.get('/', function(req, res) {
     //res.render('index.html');
-    res.sendFile(path.join(__dirname+'/public/index.html'));
+    res.sendFile(path.join(__dirname+'/public/overview.html'));
 });
 
 
@@ -21,7 +21,7 @@ router.get('/allZones', function(req, res) {
 
 /* GET overview page. */
 router.get('/overview', function(req, res) {
-    res.sendFile(path.join(__dirname.substring(0, __dirname.length - 7) +'/public/index.html'));
+    res.sendFile(path.join(__dirname.substring(0, __dirname.length - 7) +'/public/overview.html'));
 });
 
 /* GET config page. */
@@ -39,57 +39,40 @@ router.get('/users', function(req, res) {
     res.sendFile(path.join(__dirname.substring(0, __dirname.length - 7) +'/public/users.html'));
 });
 
-// var getTimes = function (json) {
-//     console.log("Getting times from json");
-//     var times = "[";
-//     for(var i = 0; i < 3; i++) {
-//         //console.log(json.times[i].begin);
-//         console.log("Time at " + i + " : " + json);
-//        // console.log(json.times[i][begin]);
-//         var begin = json.times[i].begin;
-//         var end = json.times[i].end;
-//         times += "{\"begin\": " + begin + " , \"end\" : " + end + " }";
-//         if(i < 2) times += ", ";
-//     }
-//     times += "]";
-
-//     return times;
-// };
-
 /* POST to Config */
 router.post('/config', function(req, res) {
     var db = req.db;
 
-    var zone = req.body;
-    console.log(zone);
-    ///var times = getTimes(req.body);
+    var zone = req.body.zone;
+    var size = req.body.count;
 
-    console.log("Times: " + zone.times);
-    var times = "[";
-    for(var i = 0; i < 3; i++) {
-        //console.log(timeArray[i]);
-       // console.log(timeArray[i]);
-        // var begin = req.body.times[i].begin;
-        // var end = req.body.times[i].end;
-        // times += "{\"begin\": " + begin + " , \"end\" : " + end + " }";
-        // if(i < 2) times += ", ";
+    console.log(req.body);
+
+    console.log("Zone: " + zone);
+    console.log("Size: " + size);
+
+    var timesArr =[];
+    for(var i = 0; i < size; i++) {
+        var begin = req.body['times[' + i + '][begin]'];
+        var end = req.body['times['+ i + '][end]'];
+        
+        timesArr.push({"begin":begin, "end":end});
     }
-    times += "]";
-    console.log(times);
-    
-    console.log("Zone: " + zone.zone);
-    //console.log("Times: " + zone.times);
+
+    console.log(timesArr);
 
     var collection = db.get('zonecollection');
 
-    collection.update({
-        "zone" : zone.zone,
-        "times" : times
+    collection.update({"zone": zone}, 
+    { 
+        $set : { "times" : timesArr }
     }, function (err, doc) {
         if (err) {
-            result.send("There was an issue adding the information to the database.");
+            res.send("There was an issue adding the information to the database.");
+            console.log("There was an issue adding the information to the database.");
         } else {
-            result.send("Times successfully updated.");
+            res.send("Times successfully updated.");
+            console.log("Times successfully updated.");
         }
     })
 });
