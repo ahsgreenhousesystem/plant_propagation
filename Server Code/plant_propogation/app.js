750480ed1eddf5226980525ac29e9d900d05b1ca
CamJ <camj@iastate.edu>
2015-10-25 23:01:32 -0500

    change view engine to jade

--- Server Code/plant_propogation/app.js
+++ Server Code/plant_propogation/app.js
@@ -18,7 +18,7 @@ app.use(express.static(__dirname + '/public'));
 
 // view engine setup   
 app.set('views', path.join(__dirname, 'views'));   
-app.set('view engine', 'ejs');    
+app.set('view engine', 'jade');    
     
 app.use(logger('dev'));    
 app.use(bodyParser.json());    
