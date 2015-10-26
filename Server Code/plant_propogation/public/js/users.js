b5343744b6de36f3582e6ee3ba46f0dc2c097f0f
sbajric <sbajric@iastate.edu>
2015-10-26 13:22:34 -0500

    Fixed some bugs on users page

--- Server Code/plant_propogation/public/js/users.js
+++ Server Code/plant_propogation/public/js/users.js
@@ -77,8 +77,8 @@ $("#saveUser").bind("click", function() {
     var name = $("#newUserFullName");
     var email = $("#newUserEmail");
     var phone = $("#newUserPhoneNumber");
-    if (validateFields(name.val(), email.val(), phone.val())) {
-        $("#userTable").append(addUser(name, email, phone));
+    if (validateFields(name, email, phone)) {
+        $("#userTable").append(addUser(name.val(), email.val(), phone.val()));
         $("#newUserModal").modal("hide");
          $.post("/addUser", {
                 "name": name.val(),
@@ -90,24 +90,21 @@ $("#saveUser").bind("click", function() {
     }
 });
 
-function isUserNameValid(obj) {
-    var fullName = obj.val();
-    if (fullName.length < 1) {
+function isUserNameValid(name) {
+    if (name.length < 1) {
         return false;
     }
     return true;
 }
 
-function isEmailValid(obj) {
-    var emailAddress = obj.val();
+function isEmailValid(email) {
     var pattern = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
-    return pattern.test(emailAddress);
+    return pattern.test(email.val());
 }
 
-function isPhoneValid(obj) {
-    var phoneNumber = obj.val();
+function isPhoneValid(phone) {
     var pattern = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/
-    return pattern.test(phoneNumber);
+    return pattern.test(phone.val());
 }
 
 function addUser(name, email, phone) {
