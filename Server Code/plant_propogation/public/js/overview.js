eccdd098bb5e3220c3431daea2f54acf35a43850
bajric6 <saldinbajric@gmail.com>
2015-12-04 16:11:53 -0600

    Fixed some stuff

--- Server Code/plant_propogation/public/js/overview.js
+++ Server Code/plant_propogation/public/js/overview.js
@@ -42,14 +42,28 @@ $(function() {
         var zone = button.attr("name"); // button zone name
         var action = button.val(); // button value (Open/Close/Auto)
         var data = {"zone": zone, "action": action};
-
-        $.post("/control", data, function(response) { // send AJAX request to the web server
-            $("#" + zone + "log").text(response); // add notification to the log in the zone button panel
-            var zoneButtons = document.getElementsByName(zone); // array of the buttons for that zone
-            for (var i = 0; i < zoneButtons.length; i++) {
-                zoneButtons[i].setAttribute("class", "btn btn-default btn-sm"); // reset all buttons to default
-            }
-            button.addClass("btn-success"); // highlight the button that was clicked
-        });
+        
+		$.ajax({
+			url: '/control',
+			data: data,
+			method: "post",
+			success: function(response){
+		    	$("#" + zone + "log").text(response); // add notification to the log in the zone button panel
+            	var zoneButtons = document.getElementsByName(zone); // array of the buttons for that zone
+            	for (var i = 0; i < zoneButtons.length; i++) {
+                	zoneButtons[i].setAttribute("class", "btn btn-default btn-sm"); // reset all buttons to default
+            	}
+            	button.addClass("btn-success"); // highlight the button that was clicked
+			},
+			error: function(){}
+		});
+//         $.post("/control", data, function(response) { // send AJAX request to the web server
+//             $("#" + zone + "log").text(response); // add notification to the log in the zone button panel
+//             var zoneButtons = document.getElementsByName(zone); // array of the buttons for that zone
+//             for (var i = 0; i < zoneButtons.length; i++) {
+//                 zoneButtons[i].setAttribute("class", "btn btn-default btn-sm"); // reset all buttons to default
+//             }
+//             button.addClass("btn-success"); // highlight the button that was clicked
+//         });
     });
 });
