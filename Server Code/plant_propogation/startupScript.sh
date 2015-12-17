36409ff76ff63fb0eeb1d0599207b03b977c3c6e
ISU Senior Design <dec1501@iastate.edu>
2015-12-17 17:28:01 -0600

    Final startup script

--- Server Code/plant_propogation/startupScript.sh
+++ Server Code/plant_propogation/startupScript.sh
@@ -9,5 +9,6 @@
 fswebcam -d '/dev/video2' -r 1280x720 '/home/udooer/plant_propagation/Server Code/plant_propogation/public/streaming.jpg' -l 3 >/dev/null 2>&1 &
 
 # change to the right directory (path might change a little)
-cd '/home/udooer/Documents/plant_propagation/Server Code/plant_propogation'
-npm start
+cd '/home/udooer/plant_propagation/Server Code/plant_propogation'
+npm start >/dev/null 2>&1 &
+
