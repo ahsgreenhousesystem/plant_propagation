cf3610728f254bbf44926803bdcdeb007a8d0c98
ISU Senior Design <dec1501@iastate.edu>
2015-12-17 16:37:16 -0600

    Added camera to startup script.

--- Server Code/plant_propogation/startupScript.sh
+++ Server Code/plant_propogation/startupScript.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # start database (path might change a little)
-mongod --dbpath '/home/udooer/Documents/code/cpre492/dec15-01/Server Code/plant_propogation/data' &
+mongod --dbpath '/home/udooer/Documents/plant_propagation/Server Code/plant_propogation/data' &
+
+# start camera
+fswebcam -d '/dev/video2' -r 1280x720 '/home/udooer/plant_propagation/Server Code/plant_propogation/public/streaming.jpg' -l 3 >/dev/null 2>&1 &
 
 # change to the right directory (path might change a little)
-cd '/home/udooer/Documents/code/cpre492/dec15-01/Server Code/plant_propogation'
+cd '/home/udooer/Documents/plant_propagation/Server Code/plant_propogation'
 npm start
